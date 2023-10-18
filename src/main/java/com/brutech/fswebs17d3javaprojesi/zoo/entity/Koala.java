13d7ff9d088dc0768bdc5dbc1e9f832d136badbe
burgokmen <burgokmen@gmail.com>
2023-10-18 15:12:26 +0300

    koala pojo class

---
+++ src/main/java/com/brutech/fswebs17d3javaprojesi/zoo/entity/Koala.java
@@ -0,0 +1,16 @@
+package com.brutech.fswebs17d3javaprojesi.zoo.entity;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Koala {
+    private int id;
+    private String name;
+    private double weight;
+    private double sleepHour;
+    private Gender gender;
+
+
+}
