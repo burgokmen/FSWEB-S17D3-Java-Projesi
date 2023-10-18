8d92f31fcdd0f0dab0ac4f0cd172597b261f96f0
burgokmen <burgokmen@gmail.com>
2023-10-18 15:11:56 +0300

    zoo folder refactor

--- src/test/java/com/brutech/fswebs17d3javaprojesi/zoo/ZooAppTests.java
+++ src/test/java/com/brutech/fswebs17d3javaprojesi/zoo/ZooAppTests.java
@@ -1,10 +1,10 @@
-package com.brutech.fswebs17d3javaprojesi;
+package com.brutech.fswebs17d3javaprojesi.zoo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FswebS17D3JavaProjesiApplicationTests {
+class ZooAppTests {
 
     @Test
     void contextLoads() {
